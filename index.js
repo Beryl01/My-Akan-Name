d9b7bedd9cdf1d45b840deb1f5ee7419ef6d2876
Beryl01 <berylnegesa@gmail.com>
2020-02-15 12:56:49 +0300

    created var array female names  on js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function akanNames() {
     var CC = parseInt(YY - 1) / 100 + 1; //century
     var dayOfTheWeek = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7; //day of the week
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var gender;
     //gender
